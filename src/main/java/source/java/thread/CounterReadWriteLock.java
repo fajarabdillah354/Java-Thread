5248d1e0248d4b7f795275b0c1b2260e738421c2
unknown <fajardillah25@gmail.com>
2024-07-19 22:01:38 +0700

    Upload

---
+++ src/main/java/source/java/thread/CounterReadWriteLock.java
@@ -0,0 +1,37 @@
+package source.java.thread;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class CounterReadWriteLock {
+    public Long value = 0L;
+    final ReadWriteLock lock = new ReentrantReadWriteLock();
+
+    public void increment(){
+        try{
+            lock.writeLock().lock();
+            value++;
+        }finally {
+            lock.writeLock().unlock();
+        }
+    }
+
+
+    public Long getValue() {
+
+
+        try{
+            lock.readLock().lock();
+            return value;
+        }finally {
+            lock.readLock().unlock();
+        }
+
+
+    }
+
+
+
+}
